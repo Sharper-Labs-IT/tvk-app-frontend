ac3dea3a328cc38fbc82a3dd77110fd796b1d89a
Matheesha Dissanayake <SA22524904@my.sliit.lk>
2025-12-03 17:15:29 +0530

    Changed navbar in the game page

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import Header from '../components/Header';
 import Hero from '../components/game/Hero';
 import About from '../components/game/About';
 import Story from '../components/game/Story';
 import Contact from '../components/game/Contact';
 import Footer from '../components/Footer';
 import Features from '../components/game/Features';
+import NavBar from '../components/game/Navbar';
 
 const Game: React.FC = () => {
   return (
     <div className="min-h-screen w-screen bg-black text-white relative overflow-hidden">
-      <Header />
-      <main className='bg-[#dfdff0]'>
+      <main className="bg-[#dfdff0]">
+        <NavBar />
         <Hero />
         <About />
         <Features />
