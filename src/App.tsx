38236244a500450675e60d482e1daddbc0acf650
Dushmanthr <dushmantha15ranathunga@gmail.com>
2025-12-29 22:41:50 +0530

    fix:remove unused packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+//import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
 import CookieBanner from './components/CookieBanner';
 import IdleTimeout from './components/IdleTimeOut';
