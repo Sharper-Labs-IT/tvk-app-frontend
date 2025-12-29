a37e15935643eed9dcf69e74880bf9e025480cbd
Dushmanthr <dushmantha15ranathunga@gmail.com>
2025-12-29 22:52:11 +0530

    fix: file name changed IdleTimeout.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 //import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
 import CookieBanner from './components/CookieBanner';
-import IdleTimeout from './components/IdleTimeOut';
+import IdleTimeout from './components/IdleTimeout';
 import { useAuth } from './context/AuthContext';
 
 function App() {
