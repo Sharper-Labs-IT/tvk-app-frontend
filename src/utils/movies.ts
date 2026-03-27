d8dbbd8c7ffce9c3f3f6fad16e7dc11d4344dfba
Matheesha Dissanayake <SA22524904@my.sliit.lk>
2026-03-27 09:14:09 +0530

    updated

--- src/utils/movies.ts
+++ src/utils/movies.ts
@@ -2,14 +2,14 @@
 // Converted from your pokemons.js / TVK movie list. (source images preserved). :contentReference[oaicite:4]{index=4}
 
 export const TVK_MOVIES = [
-  { name: 'Ghilli', image: 'https://www.lab-1.nl/wp-content/uploads/2024/04/Ghilli-poster.jpg' },
-  { name: 'Master', image: 'https://www.filmibeat.com/fanimg/movie/18364/master-photos-images-67380.jpg' },
-  { name: 'Leo', image: 'https://wallpapercave.com/wp/wp14342027.jpg' },
-  { name: 'Beast', image: 'https://www.kerala9.com/wp-content/uploads/2022/03/beast-tamil-movie-poster-002.jpg' },
-  { name: 'Bigil', image: 'https://static.moviecrow.com/gallery/20190622/163585-bigil%203.jpg' },
-  { name: 'Mersal', image: 'https://tse3.mm.bing.net/th/id/OIP.5RdBQEba4EhmFX5wy3kFBwHaHa?rs=1&pid=ImgDetMain&o=7&rm=3' },
-  { name: 'Theri', image: 'https://m.media-amazon.com/images/M/MV5BMjE4ZmM4MjktN2JhZS00YmY2LWFkMjMtODFhZmNjMDBjM2IzXkEyXkFqcGc@._V1_.jpg' },
-  { name: 'Sarkar', image: 'https://tse3.mm.bing.net/th/id/OIP.dYSGfseMmCT5kMf6yykMFwAAAA?rs=1&pid=ImgDetMain&o=7&rm=3' },
+  { name: 'Ghilli', image: '/img/ghilli.jpg' },
+  { name: 'Master', image: '/img/master.jpg' },
+  { name: 'Leo', image: '/img/leo.jpg' },
+  { name: 'Beast', image: '/img/beast.jpg' },
+  { name: 'Bigil', image: '/img/bigil.jpg' },
+  { name: 'Mersal', image: '/img/mersal.jpg' },
+  { name: 'Theri', image: '/img/theri.jpg' },
+  { name: 'Sarkar', image: '/img/sarkar.jpg' },
 ];
 
 export const getMovies = (count = 2) => {
